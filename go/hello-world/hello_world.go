7b4f83c7abbe9999a99a543c7a2db15eda6377e8
Matt Gowie <matt@masterpoint.io>
2020-05-14 15:06:40 -0600

    Adds golang hello-world

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting provides the standard HelloWorld function for greeting others.
+package greeting
+
+// HelloWorld greets the world!
+func HelloWorld() string {
+	return "Hello, World!"
+}
